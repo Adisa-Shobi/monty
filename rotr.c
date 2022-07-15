ef18c4d27050fe9dc3824b6699f438e6a65007aa
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-07-15 12:23:51 +0000

    Task 14 Retry

--- rotr.c
+++ rotr.c
@@ -1,26 +1,25 @@
 #include "monty.h"
 
 /**
- *rotr - Rotates the stack from the bottom up
+ *rotr - Rotates the stack to the bottom
  *
- *@line_number: The current line number
+ *@line_number: the current line number
  *@stack: The stack
  *Return: void
  */
 void rotr(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	stack_t *ptr = *stack, *tmp;
+	stack_t *ptr = *stack;
 
-	if (ptr)
+	if (ptr && ptr->prev)
 	{
-		while(ptr->prev)
+		while (ptr->prev)
 		{
-			tmp = ptr->prev;
-			ptr->prev = ptr->next;
-			ptr->next = tmp;
-			ptr = tmp;
+			ptr = ptr->prev;
 		}
-		ptr->prev = ptr->next;
+		ptr->prev = *stack;
+		(*stack)->next = ptr;
+		ptr->next->prev = NULL;
 		ptr->next = NULL;
 		*stack = ptr;
 	}
